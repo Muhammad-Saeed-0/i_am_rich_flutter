27cf36730d08eb30149307e15e1ffef545257ad5
Muhammad-Saeed-0 <muhammad01111417159@gmail.com>
2022-07-15 11:32:32 +0200

    add title & image

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(
-      child: Text("Hello World!!"),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text("I am Rich"),
+          ),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://vignette.wikia.nocookie.net/battlefordreamisland/images/c/c7/Ruby_bfb.png/revision/latest?cb=20171126235807'),
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
